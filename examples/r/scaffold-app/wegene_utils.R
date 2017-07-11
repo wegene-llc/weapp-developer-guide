9d78d2630d9e2109d510ea9e55c39b879118e423
Xiaoli Wu <xrayxiaoli@gmail.com>
2017-07-11 13:15:32 +0800

    fixed R wegene-utils bug when genome sequence is longer than index

--- examples/r/scaffold-app/wegene_utils.R
+++ examples/r/scaffold-app/wegene_utils.R
@@ -1,7 +1,8 @@
 suppressMessages(library(base64enc))
 suppressMessages(library(R.utils))
 suppressMessages(library(rjson))
-
+suppressMessages(library(radmixture))
+suppressMessages(library(scales))
 unzip_genome_data <- function(inputs) {
     genome_format <- inputs$inputs$format
     genome_binary <- base64decode(inputs$inputs$data)
@@ -11,12 +12,12 @@ unzip_genome_data <- function(inputs) {
     genome_str <- paste0(genome_str[c(TRUE, FALSE)], genome_str[c(FALSE, TRUE)])
     return(list(genome_str = genome_str, genome_format = genome_format))
 }
-
 genome_info <- function(genome_str, genome_format) {
     # Index files for all posible formats will be provided automatically
     # Do not change the default path below if you wish to use those
     index_file <- read.table(file = paste('./indexes/index_', genome_format, '.idx', sep = '', collapse = ''))
-    genome_info <- cbind(index_file[, -1], genome_str)
+    #genome_info <- cbind(index_file[, -1], genome_str)
+    genome_info <- cbind(index_file[, -1], genome_str[1:length(index_file$V1)])
     colnames(genome_info) <- c('rsid', 'chromosome', 'position', 'genotype')
     return(genome_info)
 }
