e6bb499f2882c7a6a3bfa72486caea59b2e5d8b5
Xiaoli Wu <wxl@wegene.com>
2019-12-10 19:14:51 +0800

    FIX: Py3 Util 未导出

--- examples/python3/scaffold-app/wegene_utils.py
+++ examples/python3/scaffold-app/wegene_utils.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__all__ = ['process_raw_genome_data', 'is_genotype_exist']
+__all__ = ['process_raw_genome_data', 'is_genotype_exist', 'is_wegene_format']
 
 import sys
 import gzip
